eed5fc3eb899e4e12cbfafdf8c31b93ef01d0843
Tack <45548699+suttipong02@users.noreply.github.com>
2024-02-13 21:32:30 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+include 'header.php';
+?>
+
+<body>
+    <div class="row text-white">
+        <div class="col-xl-7">
+            <img src="https://via.placeholder.com/573x480/?text=BANNER" alt="" class="w-100">
+        </div>
+        <div class="col-xl-5" style="padding-top:270px">
+            <div class="w-50 mx-auto">
+                <form action="login.php" method="post">
+                    <h1 class="text-white mb-5 bl-1 pl-4">HORRORSCOPE</h1>
+                    <div>
+                        <label for="username">Username:</label>
+                        <input type="text" id="username" name="username" required class="form-control">
+                    </div>
+                    <div class="mt-3">
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" required class="form-control">
+                    </div>
+                    <div class="row mt-4 justify-content-center">
+                        <div class="col-xl-6 mt-2">
+                            <a href="#">คุณลืมรหัสผ่าน ?</a>
+                        </div>
+                        <div class="col-xl-6">
+                            <button type="submit" class="btn btn-warning text-white rounded btn-block">เข้าสู่ระบบ</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        <?php echo $message; ?>
+    </script>
+
+</body>
+
+</html>
